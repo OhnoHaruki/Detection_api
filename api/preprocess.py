f395dfe03cf9579213795c7af89797514970b654
OhnoHaruki <haruki20010219@gmail.com>
2022-10-06 15:38:09 +0900

    データ準備、前処理、後処理の実装

---
+++ api/preprocess.py
@@ -0,0 +1,10 @@
+"""
+データの前処理p.322
+"""
+
+import torchvision
+
+def image_to_tensor(image):
+    #画像データをテンソル型へ変換
+    image_tensor = torchvision.transforms.functional.to_tensor(image)
+    return image_tensor
